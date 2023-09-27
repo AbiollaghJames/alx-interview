5d0df052bebdf275c17b2f4276320475150a01dc
AbiollaghJames <abiollajames@protonmail.com>
2023-09-27 16:00:55 +0300

    Pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,16 +3,23 @@
 
 def pascal_triangle(n):
     """function that creates pascals triangle"""
+
     if n <= 0:
         return []
+
     lista = []
+
     for i in range(n):
         listb = []
+
         for j in range(i+1):
             if j == 0 or j == i:
                 listb.append(1)
+
             else:
                 m_value = lista[i-1][j] + lista[i-1][j-1]
                 listb.append(m_value)
+
         lista.append(listb)
+
     return lista
