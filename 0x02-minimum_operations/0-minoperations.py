a2b34696649bf6c9a1d583ef035da86b265d5852
AbiollaghJames <abiollajames@protonmail.com>
2023-10-13 05:48:29 +0300

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Finding minimum number of operations to obtain
+`n` H characters using `Copy All` and `Paste` operations
+"""
+
+
+def minOperations(n):
+    """
+    Takes int n as input and returns int which reps
+    number of min operations needed to achieve n H chars
+    """
+    if n < 2:
+        return 0 #impossible to achieve n < 2 H chars
+
+    ops = 0
+    factor = 2
+
+    while factor * factor <= n:
+        while n % factor == 0:
+            n //= factor
+            ops += factor
+        factor += 1
+
+    if n > 1:
+        ops += n
+    return ops
