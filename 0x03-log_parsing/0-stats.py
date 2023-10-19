ad3f31b57c1d1a1827dd667278b7c3625051d1b5
AbiollaghJames <abiollajames@protonmail.com>
2023-10-19 09:05:36 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,42 +12,33 @@ import sys
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
+s_code = 0
+
+def to_print(status_codes, total_size):
+    print(f"File size: {total_size}")
+    for k, v in sorted(status_codes.items()):
+        if v != 0:
+            print(f"{k}: {v}")
 
 try:
-    """
-    read stdin line by line and split on space
-    if length of line > 8/not the format, skip the line and move
-    to the next line
-    """
     for line in sys.stdin:
         line_parts = line.split()
+        line_parts = line_parts[::-1]
 
-        if len(line_parts) < 8:
-            continue
-        f_size = int(line_parts[-1])
-        status_code = int(line_parts[-2])
+        if len(line_parts) > 2:
+            line_count += 1
 
-        """
-        compute file size and capture status codes count
-        """
-        total_size += f_size
-        if status_code in status_codes:
-            status_codes[status_code] += 1
+        if line_count <= 10:
+            total_size += int(line_parts[0])
+            s_code = line_parts[1]
 
-        line_count += 1
+            if (s_code in status_codes.keys()):
+                status_codes[s_code] += 1
 
-        """
-        print line count after every 10 lines and sorted in
-        ascending order
-        """
-        if line_count % 10 == 0:
-            print("File size: ", total_size)
+            
+            if (line_count == 10):
+                to_print(status_codes, total_size)
+                line_count = 0
 
-            for s_code in sorted(status_codes.keys()):
-                if status_codes[s_code] > 0:
-                    print(f"{s_code}: {status_codes[s_code]}")
-
-except KeyboardInterrupt:
-    print("File size: ", total_size)
-    for s_code in sorted(status_codes.keys()):
-        print(f"{s_code}: {status_codes[s_code]}")
+finally:
+    to_print(status_codes, total_size)
