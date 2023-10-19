3d26a32745219b26c893136f24d4df9fc2a0f833
AbiollaghJames <abiollajames@protonmail.com>
2023-10-19 09:22:49 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,7 +8,6 @@ the beginning
 
 import sys
 
-
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
@@ -25,21 +24,18 @@ def to_print(status_codes, total_size):
 try:
     for line in sys.stdin:
         line_parts = line.split()
-        line_parts = line_parts[::-1]
 
-        if len(line_parts) > 2:
-            line_count += 1
+        if len(line_parts) >= 8:
+            s_code = int(line_parts[-2])
+            total_size += int(line_parts[-1])
 
-        if line_count <= 10:
-            total_size += int(line_parts[0])
-            s_code = line_parts[1]
-
-            if (s_code in status_codes.keys()):
+            if s_code in status_codes:
                 status_codes[s_code] += 1
 
-        if (line_count == 10):
-            to_print(status_codes, total_size)
-            line_count = 0
+            line_count += 1
+            if line_count >= 10:
+                to_print(status_codes, total_size)
+                line_count = 0
 
 finally:
     to_print(status_codes, total_size)
