8ad8be6f7f0f7a789b0e892389fadd6f713b08fc
AbiollaghJames <abiollajames@protonmail.com>
2023-10-24 07:41:20 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,29 +15,22 @@ def validUTF8(data):
     num_bytes = 0
 
     for byte in data:
-        """convert byte to binary"""
         byte_to_binary = format(byte, '08b')
 
         if num_bytes == 0:
-            """new byte in the sequence"""
             if byte_to_binary.startswith('0'):
-                """single byte char"""
                 num_bytes = 0
             elif byte_to_binary.startswith('110'):
-                """2-bytes char"""
                 num_bytes = 1
             elif byte_to_binary.startswith('1110'):
-                """3-bytes char"""
                 num_bytes = 2
             elif byte_to_binary.startswith('11110'):
-                """4-bytes char"""
                 num_bytes = 3
             else:
                 return False
         else:
             if not byte_to_binary.startswith('10'):
-                """continuation byte"""
                 return False
             num_bytes -= 1
-        """complete bytes"""
+
     return num_bytes == 0
