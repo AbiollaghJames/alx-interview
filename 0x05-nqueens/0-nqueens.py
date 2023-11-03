07d24fa3b03dda7cb1d16e45a6a0d219662c7f1b
AbiollaghJames <abiollajames@protonmail.com>
2023-11-03 05:32:43 +0300

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,71 +3,49 @@
 N Queens module
 """
 
-import sys
+from sys import argv
 
 
-def is_safe(board, row, col, n):
-    """
-    Checks if it's safe to place a queen in a
-    particular position
-    """
-    for i in range(row):
-        if board[i][col] == 1:
+def n_queens(cell: list) -> bool:
+    """ returns bool if n queens """
+    row_num = len(cell) - 1
+    diff = 0
+    for i in range(0, row_num):
+        diff = cell[i] - cell[row_num]
+        if diff < 0:
+            diff *= -1
+        if diff == 0 or diff == row_num - i:
             return False
-
-        for j in range(col):
-            if board[i][j] == 1:
-                if i + j == row + col or i - j == row - col:
-                    return False
-
     return True
 
 
-def n_queens(n):
-    """
-    Ensures n >= 4
-    """
-    if n < 4:
-        return []
-
-    def backtrack(row):
-        """
-        Backtracking algorithm
-        """
-        if row == n:
-            solution = [list(row) for row in board]
-            sols.append(solution)
-            return
-        for col in range(n):
-            if is_safe(board, row, col, n):
-                board[row][col] = 1
-                backtrack(row + 1)
-                board[row][col] = 0
-
-    sols = []
-    board = [[0 for r in range(n)] for c in range(n)]
-    backtrack(0)
-    return sols
+def solve_n_queens(d: int, row: int, cell: list, output: list):
+    """ gets result of N Queens recursively """
+    if row == d:
+        print(output)
+    else:
+        for col in range(0, d):
+            cell.append(col)
+            output.append([row, col])
+            if (n_queens(cell)):
+                solve_n_queens(d, row + 1, cell, output)
+            cell.pop()
+            output.pop()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
     try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
+        N = int(argv[1])
+    except BaseException:
+        print('N must be a number')
+        exit(1)
     if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    sols = n_queens(N)
-
-    for sol in sols:
-        for row in sol:
-            print(row)
-        print()
+        print('N must be at least 4')
+        exit(1)
+    else:
+        output = []
+        cell = 0
+        solve_n_queens(int(N), cell, [], output)
