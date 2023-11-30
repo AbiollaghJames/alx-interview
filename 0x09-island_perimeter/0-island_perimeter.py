2ee74f66edb3f5c43d650da1c4911dc21b3ee88c
AbiollaghJames <abiollajames@protonmail.com>
2023-11-30 20:20:30 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter function
+    """
+    if not grid or not grid[0]:
+        return 0
+
+    per = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                per += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    per -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    per -= 2
+
+    return per
